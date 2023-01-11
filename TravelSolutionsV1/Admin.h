428a0f6b5ff6830866dfc1a16dd07bedf0830d52
kosovact14 <tamarakosovac14@gmail.com>
2023-01-11 19:56:40 +0100

    Dodavanje podataka od strane admin(popravljena verzija)

--- TravelSolutionsV1/Admin.h
+++ TravelSolutionsV1/Admin.h
@@ -33,212 +33,187 @@ public:
 
 	void addTour()
 	{
-		std::string startLocation, endLocation, startTime, endTime, withinBorder;
-		bool check1, check2;
-		system("cls");
-		std::fstream file;
-
-		file.open("Tours.txt", std::ios::app);
-		if (file.is_open()) {
-			do {
-				std::cout << "Unesite pocetnu lokaciju ture:\n";
-				std::cin >> startLocation;
-				std::cout << "Unesite krajnju lokaciju ture:\n";
-				std::cin >> endLocation;
-				check1 = checkStartLocation(startLocation);
-				if (check1 == true)
-					file << startLocation << " - ";
-				else {
-					system("cls");
-					std::cout << "Unesena lokacija se ne moze dodati u turu. " << std::endl;
-				}
-
-				check2 = checkEndLocation(endLocation);
-				if (check2 == true)
-					file << endLocation << "  ";
-				else {
-					system("cls");
-					std::cout << "Unesena lokacija se ne moze dodati u turu. " << std::endl;
-				}
-			} while (check1 == false && check2 == false);
-
-			std::cout << "Unesite vrijeme polaska:\n";
-			std::cin >> startTime;
-			std::cout << "Unesite vrijeme dolaska:\n";
-			std::cin >> endTime;
-			std::cout << "Da li je tura unutar granice drzave? (Unesite Da ili Ne) \n";
-			std::cin >> withinBorder;
-
-			file << startTime << " - " << endTime << "  ";
-
-			if (withinBorder == "Da") {
-				file << "Tura je unutar granice drzave!" << std::endl;
+		std::string id, startTime, endTime, withinBorder;
+		int num;
+		bool check, checkId;
+		std::fstream tourFile;
+		std::cout << "Unesite jedinstveni ID ture:\n";
+		std::cin >> id;
+		do {
+			checkId = check_id_tour(id);
+			if (checkId == true) {
+				tourFile.open("Tours.txt", std::ios::app);
+				tourFile << id << " ";
 			}
 			else {
-				file << "Tura je izvan granice drzave!" << std::endl;
+				std::cout << "Zahtijeva se ponovni unos ID-a:\n";
+				std::cin >> id;
 			}
-			file.close();
+		} while (checkId == false);
+		std::cout << "Unesite koliko lokacija ce imati tura:\n";
+		std::cin >> num;
+		tourFile << num << " ";
+		system("cls");
+		do {
+			std::string location;
+			std::cout << "Unesite lokaciju koju zelite da dodate u turu:\n";
+			std::cin >> location;
+			check = checkLocation(location);
+			if (check == true) {
+				tourFile << location << " ";
+				num--;
+			}
+			else
+				std::cout << "Lokaciju nije moguce dodati u turu.\n";
+		} while (num);
+
+		std::cout << "Unesite vrijeme polaska:\n";
+		std::cin >> startTime;
+		std::cout << "Unesite vrijeme dolaska:\n";
+		std::cin >> endTime;
+		std::cout << "Da li je tura unutar granice drzave? (Unesite Da ili Ne) \n";
+		std::cin >> withinBorder;
+		tourFile << startTime << "  " << endTime << "  ";
+		if (withinBorder == "Da") {
+			tourFile << "1" << std::endl;
 		}
+		else {
+			tourFile << "0" << std::endl;
+		}
+		tourFile.close();
 	}
 
 	void addBus()
 	{
-		std::string model, hasWifi, hasWc;
+		std::string id, model, hasWifi, hasWc, isAvailable = "true";
 		int numOfSeats;
-		system("cls");
+		bool checkId;
+		std::fstream busFile;
+		std::cout << "Unesite jedinstveni ID busa:\n";
+		std::cin >> id;
+		do {
+			checkId = check_id_bus(id);
+			if (checkId == true) {
+				busFile.open("Bus.txt", std::ios::app);
+				busFile << id << " ";
+			}
+			else {
+				std::cout << "Zahtijeva se ponovni unos id-a:\n";
+				std::cin >> id;
+			}
+		} while (checkId == false);
 		std::cout << "Unesite model autobusa:\n";
 		std::cin >> model;
 		std::cout << "Unesite broj sjedista:\n";
 		std::cin >> numOfSeats;
-		std::cout << "Da li autobus ima wifi? (Unesite Da ili Ne.)\n";
+		std::cout << "Da li autobus ima wifi? (Unesite Da ili Ne)\n";
 		std::cin >> hasWifi;
-		std::cout << "Da li autobus ima wc? (Unesite Da ili Ne.)\n";
+		std::cout << "Da li autobus ima wc? (Unesite Da ili Ne)\n";
 		std::cin >> hasWc;
-		std::fstream file;
-		file.open("Bus.txt", std::ios::app);
-		if (file.is_open()) {
-			file << model << "  " << numOfSeats << "  ";
-			if (hasWifi == "Da") {
-				file << "Autobus ima wifi." << "  ";
-			}
-			else
-				file << "Autobus nema wifi." << "  ";
-
-			if (hasWc == "Da") {
-				file << "Autobus ima wc." << std::endl;
-			}
-			else
-				file << "Autobus nema wc." << std::endl;
-
-			file.close();
+		busFile << model << "  " << numOfSeats << "  ";
+		if (hasWifi == "Da") {
+			busFile << "1" << "  ";
 		}
+		else
+			busFile << "0" << "  ";
+
+		if (hasWc == "Da") {
+			busFile << "1" << " " << isAvailable << std::endl;
+		}
+		else
+			busFile << "0" << " " << isAvailable << std::endl;
+
+		busFile.close();
 	}
 
 	void addLocation()
 	{
-		system("cls");
-		std::string startLocation, endLocation;
-		bool check1, check2;
+		std::string location;
+		bool check;
+		std::fstream locationFile;
 		do {
-			std::cout << "Unesite pocetnu lokaciju:\n";
-			std::cin >> startLocation;
-			check1 = checkStartLocation(startLocation);
-			if (check1 == false) {
-				std::fstream file;
-				file.open("startLocations.txt", std::ios::app);
-				if (file.is_open()) {
-					file << startLocation;
-					file.close();
-				}
+			std::cout << "Unesite lokaciju koju zelite da dodate:\n";
+			std::cin >> location;
+			check = checkLocation(location);
+			if (check == false) {
+				locationFile.open("Locations.txt", std::ios::app);
+				if (locationFile.is_open())
+					locationFile << location << "\n";
 			}
-			else
-			{
-				system("cls");
-				std::cout << "Lokacija vec postoji." << std::endl;
-			}
-
-			std::cout << "Unesite krajnju lokaciju:\n";
-			std::cin >> endLocation;
-			check2 = checkEndLocation(endLocation);
-			if (check2 == false) {
-				std::fstream file;
-				file.open("endLocations.txt", std::ios::app);
-				if (file.is_open()) {
-					file << endLocation;
-					file.close();
-				}
-			}
-			else {
-				system("cls");
-				std::cout << "Lokacija vec postoji." << std::endl;
-			}
-
-
-		} while (check1 == true && check2 == true);
+		} while (check == true);
 	}
 
 	void addDrive()
 	{
-		system("cls");
-		std::string tour, driver, bus;
-		std::fstream file;
-		bool check = false;
+		std::string idTour, username, idBus;
+		std::fstream file1, file2, file3;
+		bool checkId1, checkId2, checkId3;
 		do
 		{
-			std::cout << "Unesite turu:\n";
-			std::cin >> tour;
-			std::string line;
-			file.open("Tours.txt", std::ios::in);
-			if (file.is_open()) {
-				while (getline(file, line))
-				{
-					if (tour == line) {
-						check = true;
-						file.open("Drive.txt", std::ios::app);
-						file << tour << "  "; break;
-					}
-				}
-				file.close();
-			}
-		} while (check==false);
-		bool check1 = false;
-		do
-		{
-			std::cout << "Unesite vozaca:\n";
-			std::cin >> driver;
-			std::string line;
-			file.open("Users.txt", std::ios::in);
-			if (file.is_open()) {
-				std::string line;
-				while (getline(file, line))
-				{
-					if (driver == line) {
-						check1 = true;
-						file.open("Drive.txt", std::ios::app);
-						file << driver << "  "; break;
-					}
-				}
-				file.close();
-			}
-		} while (check1 == false);
-		bool check2 = false;
-		do
-		{
-			std::cout << "Unesite autobus:\n";
-			std::cin >> bus;
-			std::string line;
-			file.open("Bus.txt", std::ios::in);
-			if (file.is_open()) {
-				while (getline(file, line))
-				{
-					if (bus == line) {
-						file.open("Drive.txt", std::ios::app);
-						file << bus << "\n"; break;
-					}
+			std::cout << "Unesite jedinstveni ID ture:\n";
+			std::cin >> idTour;
+			checkId1 = check_idTour_exist(idTour);
+			if (checkId1)
+			{
+				file1.open("Drive.txt", std::ios::app);
+				file1 << idTour << " ";
 
-				}
-				file.close();
 			}
-		} while (check2==false);
+			else
+				std::cout << "Uneseni ID ne postoji." << std::endl;
+
+		} while (checkId1 == false);
+
+		do
+		{
+			std::cout << "Unesite username vozaca:\n";
+			std::cin >> username;
+			checkId2 = (check_username_exist(username) && check_is_driver_available(username));
+			if (checkId2)
+			{
+				file2.open("Drive.txt", std::ios::app);
+				file2 << username << " ";
+				driverIsBusy(username);
+			}
+			else
+				std::cout << "Uneseni username ne postoji." << std::endl;
+		} while (checkId2 == false);
+
+		do
+		{
+			std::cout << "Unesite jedinstveni ID busa:\n";
+			std::cin >> idBus;
+			checkId3 = (check_idBus_exist(idBus) && check_is_bus_available(idBus));
+			if (checkId3)
+			{
+				file3.open("Drive.txt", std::ios::app);
+				file3 << idBus << std::endl;
+				busIsBusy(idBus);
+			}
+			else
+				std::cout << "Uneseni ID ne postoji." << std::endl;
+
+		} while (checkId3 == false);
+
 	}
 
 	void addCoupon()
 	{
 		char alpha[73] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n','o', 'p', 'q', 'r',
-		's', 't', 'u', 'v', 'w', 'x', 'y', 'z' , 'A' , 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
+		's', 't', 'u', 'v', 'w', 'x', 'y', 'z' , 'A' , 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
 		'N','O', 'P', 'Q', 'R','S', 'T','U', 'V', 'W','X','Y','Z', '0', '1','2','3','4','5','6','7','8','9', '/',
-		'*','-','+','(', ')', '%', '&', '=', '!', '?'};
+		'*','-','+','(', ')', '%', '&', '=', '!', '?' };
 
 		std::string result = "", value;
 		for (int i = 0; i < 10; i++)
 			result = result + alpha[rand() % 73];
 		std::cout << "Unesite vrijednost bona:\n";
 		std::cin >> value;
-		std::fstream file;
-		file.open("Bonovi.txt", std::ios::app);
-		if (file.is_open()) {
-			file << result << "  " << value << "\n";
-			file.close();
+		std::fstream couponFile;
+		couponFile.open("Coupons.txt", std::ios::app);
+		if (couponFile.is_open()) {
+			couponFile << result << "  " << value << "\n";
+			couponFile.close();
 		}
 	}
 
@@ -297,12 +272,12 @@ public:
 					else
 					{
 						system("cls");
-						std::cout << "Izabrali ste nepostojecu opciju. Pokusajte ponovo: " << std::endl;
+						std::cout << "Izabrali ste nepostojecu opciju. Pokusajte ponovo " << std::endl;
 					}
 				} while (option1 != "1" && option1 != "2" && option1 != "3" && option1 != "4" && option1 != "5");
 			}
 			else if (option == "2")
-			{  
+			{
 				std::string option2;
 				system("cls");
 				do
@@ -349,10 +324,10 @@ public:
 					else
 					{
 						system("cls");
-						std::cout << "Izabrali ste nepostojecu opciju. Pokusajte ponovo: " << std::endl;
+						std::cout << "Izabrali ste nepostojecu opciju. Pokusajte ponovo " << std::endl;
 					}
-				} while (option2 != "1"&& option2!="2" && option2 !="3" && option2!="4" && option2 !="5" && option2!="6");
-			} 
+				} while (option2 != "1" && option2 != "2" && option2 != "3" && option2 != "4" && option2 != "5" && option2 != "6");
+			}
 			//else {}
 
 		} while (option != "1" && option != "2" && option != "3");
--- TravelSolutionsV1/validate.h
+++ TravelSolutionsV1/validate.h
@@ -62,6 +62,32 @@ bool check_password(std::string forcheck)
 	return 0;
 }
 
+//PROVJERA VALIDNOSTI ID-A TURE
+bool check_id_tour(std::string forcheck)
+{
+	for (int i = 0; i < forcheck.size(); i++)
+	{
+		if (((isdigit(forcheck[i])) || (isalpha(forcheck[i]))) && (forcheck.size() > 4))
+		{
+			return 1;
+		}
+		return 0;
+	}
+}
+
+//PROVJERA VALIDNOSTI ID-A BUSA
+bool check_id_bus(std::string forcheck)
+{
+	for (int i = 0; i < forcheck.size(); i++)
+	{
+		if ((isdigit(forcheck[i])) && (forcheck.size() < 3))
+		{
+			return 1;
+		}
+		return 0;
+	}
+}
+
 //VRACA TIP KORISNIKA( DRIVER, ADMIN, USER)
 std::string getUserType(std::string username)
 {
@@ -118,40 +144,196 @@ bool userExist(std::string username, std::string password)
 	return false;
 }
 
-bool checkStartLocation(std::string startlocation)
+//PROVJERA DA LI TURA POSTOJI 
+bool check_idTour_exist(std::string id)
 {
-	std::fstream file;
+	std::fstream tFile("Tours.txt", std::ios::in);
+	int n = num_of_lines("Tours.txt");
+	std::string out1, out2, out3;
+	for (int i = 0; i < n; i++)
+	{
+		tFile >> out1;
+		if (out1 == id)
+			return true;
+		tFile >> out2;
+		int x = stoi(out2);
+		int y = x + 3;
+		for (int j = 0; j < y; j++)
+		{
+			tFile >> out3;
+		}
+	}
+	return false;
+}
+
+//PROVJERA DA LI VOZAC POSTOJI U SISTEMU
+bool check_username_exist(std::string username)
+{
+	std::fstream uFile("Users.txt", std::ios::in);
+	int n = num_of_lines("Users.txt");
+	std::string out;
+	for (int i = 0; i < n; i++)
+	{
+		int check = 0;
+		for (int j = 0; j < 7; j++)
+		{
+			uFile >> out;
+			if ((j == 2) && (out == username))
+			{
+				check++;
+			}
+			if (check == 1)
+				return true;
+		}
+	}
+	return false;
+}
+
+//PROVJERA DA LI BUS POSTOJI
+bool check_idBus_exist(std::string id)
+{
+	std::fstream bFile("Bus.txt", std::ios::in);
+	int n = num_of_lines("Bus.txt");
+	std::string out;
+	for (int i = 0; i < n; i++)
+	{
+		int check = 0;
+		for (int j = 0; j < 6; j++)
+		{
+			bFile >> out;
+			if ((j == 0) && (out == id))
+				check++;
+			if (check == 1)
+				return true;
+		}
+	}
+	return false;
+
+}
+
+//PROVJERA DA LI JE VOZAC DOSTUPAN
+bool check_is_driver_available(std::string username)
+{
+	std::fstream dFile("Users.txt", std::ios::in);
+	int n = num_of_lines("Users.txt");
+	std::string out;
+	for (int i = 0; i < n; i++)
+	{
+		int check = 0;
+		for (int j = 0; j < 7; j++)
+		{
+			dFile >> out;
+			if ((j == 2) && (username == out))
+				check++;
+			if ((j == 6) && (out == "false"))
+				check++;
+			if (check == 2)
+				return true;
+		}
+	}
+	return false;
+}
+
+//PROVJERA DA LI JE BUS DOSTUPAN
+bool check_is_bus_available(std::string id)
+{
+	std::fstream aFile("Bus.txt", std::ios::in);
+	int n = num_of_lines("Bus.txt");
+	std::string out;
+	for (int i = 0; i < n; i++)
+	{
+		int check = 0;
+		for (int j = 0; j < 6; j++)
+		{
+			aFile >> out;
+			if ((j == 5) && (id == "true"))
+				return true;
+		}
+	}
+	return false;
+}
+
+//PROVJERA DA LI LOKACIJA POSTOJI U SISTEMU
+bool checkLocation(std::string location)
+{
+	std::fstream lFile;
 	std::string line;
 	bool check = false;
-	file.open("startLocations.txt", std::ios::in);
-	if (file.is_open()) {
-		
-		while (getline(file, line))
+	lFile.open("Locations.txt", std::ios::in);
+	if (lFile.is_open()) {
+
+		while (getline(lFile, line))
 		{
-			if (line == startlocation)
+			if (line == location)
 				check = true;
 		}
-		file.close();
+		lFile.close();
 
 		return check;
 	}
 }
 
-bool checkEndLocation(std::string endlocation)
+//POSTAVLJA STANJE VOZACA NA ZAUZETO
+void driverIsBusy(std::string username)
 {
-	std::fstream file;
-	std::string line;
+	std::fstream drFile("Users.txt", std::ios::in);
+	int n = num_of_lines("Users.txt");
+	std::string string1, string2, string3, string4, string5, string6, string7;
 	bool check = false;
-	file.open("endLocations.txt", std::ios::in);
-	if (file.is_open()) {
+	for (int i = 0; i < n; i++)
+	{
+		drFile >> string1;
+		drFile >> string2;
+		drFile >> string3;
+		if (string3 == username)
+			check = true;
+		drFile >> string4;
+		drFile >> string5;
+		drFile >> string6;
+		drFile >> string7;
+		drFile << string1;
+		drFile << string2;
+		drFile << string3;
+		drFile << string4;
+		drFile << string5;
+		drFile << string6;
+		if (check)
+			drFile << "true";
+		drFile << string7;
 
-		while (getline(file, line))
-		{
-			if (line == endlocation)
-				check = true;
-		}
-		file.close();
-		
-		return check;
 	}
 }
+
+//POSTAVLJA BUS NA STANJE GDJE JE NEDOSTUPAN
+void busIsBusy(std::string id)
+{
+	std::fstream bFile("Bus.txt", std::ios::in);
+	int n = num_of_lines("Bus.txt");
+	std::string s1, s2, s3, s4, s5, s6;
+	bool check = false;
+	for (int i = 0; i < n; i++)
+	{
+
+		bFile >> s1;
+		if (s1 == id)
+			check == true;
+		bFile >> s2;
+		bFile >> s3;
+		bFile >> s4;
+		bFile >> s5;
+		bFile >> s6;
+		bFile << s1;
+		bFile << s2;
+		bFile << s3;
+		bFile << s4;
+		bFile << s5;
+		if (check)
+			bFile << "false";
+		else
+			bFile << s6;
+
+	}
+}
+
+
+
